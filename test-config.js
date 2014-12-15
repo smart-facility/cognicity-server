cb5f335882659125dc773a70bba3ef184518403a
Ben Jones <ben.jones@selera.com>
2014-12-15 12:35:07 +1100

    Added test for server.js launch
    - npm test now runs mocha
    - Added test-config for mocha launch test
    - Added test which launches server.js expecting error because DB
    configuration is invalid (much of the code in server.js is executed and
    so we test for some types of error by doing this)

---
+++ test-config.js
@@ -0,0 +1,14 @@
+'use strict';
+
+//test-config.js - require sample config and change some properties for test
+
+var config = require("./config.js");
+
+// Change instance name so we log to test.log
+config.instance = 'test';
+
+// Log into application directory
+config.logger.logDirectory = null;
+
+// Export our modified config
+module.exports = config;
