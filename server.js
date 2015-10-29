6785be617a4189f4e9ea9671b0c47875c96d9dde
Matthew Berryman <mberryma@uow.edu.au>
2015-10-29 13:26:11 +1100

    Fixes #22

--- server.js
+++ server.js
@@ -145,7 +145,8 @@ if (config.data === true){
 
 	// Depreciate data API v1
 	app.get('/'+config.url_prefix+'/data/api/v1*',function(req, res, next){
-		res.redirect(303, '/'+config.url_prefix+'/data/api/v2'+req.params[0]);
+		res.redirect(301, '/'+config.url_prefix+'/data/api/v2'+req.params[0]);
+		res.setHeader('Cache-Control','max-age=60');
 	});
 
 	app.get( new RegExp('/'+config.url_prefix+'/data/api/v2/.*'), function(req, res, next){
